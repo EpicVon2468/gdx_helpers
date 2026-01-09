ef5bd13df1c5af1d09c02299442f36ca274785eb
EpicVon2468 <thomaschad0909@gmail.com>
2026-01-10 02:28:18 +1100

    Port from Groovy to Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,8 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 plugins {
-	//kotlin("jvm") version "2.3.0"
+	kotlin("jvm") version "2.3.0"
 }
-
-if (project.parent == null) plugins.getPlugin("org.jetbrains.kotlin.jvm").apply(project)
 
 group = "io.github.epicvon2468.gdx_helpers"
 version = "1.0.0"
